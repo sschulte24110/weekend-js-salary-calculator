a850374c6e60380946eb5f3834a632a631affc85
Scott Schulte <scott.r.schulte@gmail.com>
2024-05-03 21:17:14 -0500

    Add clearEmployees function. Add and object to collect eachEmployee.

--- salary.js
+++ salary.js
@@ -8,35 +8,44 @@ let salaryTotalMonthly = 0;
 function addEmployee(event) {
   event.preventDefault();
 
-  const firstName = document.getElementById('first-name');
-  const lastName = document.getElementById('last-name');
-  const idNumber = document.getElementById('id-number');
-  const jobTitle = document.getElementById('job-title');
-  const annualSalary = document.getElementById('annual-salary');
+  const firstNameInput = document.getElementById('first-name');
+  const lastNameInput = document.getElementById('last-name');
+  const idNumberInput = document.getElementById('id-number');
+  const jobTitleInput = document.getElementById('job-title');
+  const annualSalaryInput = document.getElementById('annual-salary');
   const monthlySalary = document.getElementById('monthly-salary');
 
-  console.log(`${firstName.value} ${lastName.value}`);
+  console.log(`${firstNameInput.value} ${lastNameInput.value}`);
+
+  const eachEmployee = {
+    firstName: firstNameInput.value,
+    lastName: lastNameInput.value,
+    idNumber: idNumberInput.value,
+    jobTitle: jobTitleInput.value,
+    annualSalary: annualSalaryInput.value,
+  };
+  console.log('Employee Object', eachEmployee);
 
   document.getElementById('table-body').innerHTML += `
   <tr>
-      <td>${firstName.value}</td>
-      <td>${lastName.value}</td>
-      <td>${idNumber.value}</td>
-      <td>${jobTitle.value}</td>
-      <td>${annualSalary.value}</td>
+      <td>${firstNameInput.value}</td>
+      <td>${lastNameInput.value}</td>
+      <td>${idNumberInput.value}</td>
+      <td>${jobTitleInput.value}</td>
+      <td>${annualSalaryInput.value}</td>
       <td><button class="button-color" onclick="deleteEmployee(event)">&#x2718</button></td>
     </tr>
   `;
-  console.log(`salary is: ${annualSalary.value}`);
-  salaryTotalMonthly += Number(annualSalary.value);
+  console.log(`salary is: ${annualSalaryInput.value}`);
+  salaryTotalMonthly += Number(annualSalaryInput.value);
 
-  monthlySalary.innerText = Math.round((salaryTotalMonthly / 12) * 100);
+  monthlySalary.innerText = Math.round(salaryTotalMonthly / 12);
 
-  firstName.value = '';
-  lastName.value = '';
-  idNumber.value = '';
-  jobTitle.value = '';
-  annualSalary.value = '';
+  firstNameInput.value = '';
+  lastNameInput.value = '';
+  idNumberInput.value = '';
+  jobTitleInput.value = '';
+  annualSalaryInput.value = '';
 }
 
 function appendEmployee() {}
